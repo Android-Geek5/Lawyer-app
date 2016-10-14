c0d7f07d9ba968ea3919e2d0a04431c8ac961919
Paramjeet Kaur <paramjeet.kaur@erginus.com>
2016-10-14 19:00:50 +0530

    commit

--- app/src/main/java/com/erginus/lawyerapp/common/MapAppConstant.java
+++
@@ -1,8 +0,0 @@
-package com.erginus.lawyerapp.common;
-
-/**
- * Created by paramjeet on 28/9/15.
- */
-public class MapAppConstant {
-    public static String API="http://erginus.net/lawyer_web/api/";
-}
